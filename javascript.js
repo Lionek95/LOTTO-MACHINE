9ccff3125031ebb14402ad1f57124b94a697f445
Kasia Jakubiec <kasiajakubiec@Air-Kasia.home>
2020-05-20 21:34:47 +0200

    lotto machine web app

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const result = [];
+
+    const draw = function() {
+      if (result.length === 6) return;
+      const score = Math.floor(Math.random() * 49 + 1);
+
+      for (let i = 0; i < result.length; i++) {
+        if (score === result[i]) {
+          return draw()
+        }
+      }
+      const div = document.createElement('div');
+      div.textContent = score;
+      
+      result.push(score); 
+      
+      document.body.appendChild(div);
+    }
+
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', draw);
